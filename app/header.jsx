adb41d356d373db093fbeffb5db901975e090ede
Jasper Oh <ohyj0906@gmail.com>
2023-05-06 00:20:21 -0700

    Logo v0.1 finalized

--- app/header.jsx
+++ app/header.jsx
@@ -2,11 +2,13 @@
 
 import Link from 'next/link'
 import LoginButton from './components/auth/SignInButton'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className='bg-stone-100 py-10'>
-      <nav className='container flex items-center text-sm font-medium tracking-wider uppercase text-stone-500'>
+      <nav className='container flex items-center text-sm font-medium tracking-wider uppercase text-stone-500 bg-[#1b2a33]'>
+      <Image src="/SAM_Logo_Final.png" alt='logo' width={97} height={93}/>
         {/* <ul className='ml-auto flex justify-center gap-8'>
           <li>
             <Link href='/'>Home</Link>
