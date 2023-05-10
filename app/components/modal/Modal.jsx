fa602c183338f56b2923910a3aad603349a5c5af
Juan Chung <juanchung7v@gmail.com>
2023-05-10 09:32:32 -0700

    added close modal button

--- app/components/modal/Modal.jsx
+++ app/components/modal/Modal.jsx
@@ -3,7 +3,6 @@ import Button from './../button/Button'
 
 const Modal = ({ }) => {
   return (
-    // Insert "hidden" in the className 
     <div tabindex="-1" aria-hidden="true" className="fixed top-15 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto max-h-full">
         <div className="relative max-w-full max-h-full">
             
@@ -13,6 +12,10 @@ const Modal = ({ }) => {
                     <h3 className="text-xl font-mono text-gray-900 dark:text-white">
                         Title
                     </h3>
+                    <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                    </button>
                 </div>
 
                 <div className="p-6 space-y-6">
@@ -30,7 +33,6 @@ const Modal = ({ }) => {
                     < Button text="AI Check" />
                     < Button text="AI Generate" />
                     < Button text="Text-to-Speech" />
-
                     < Button text="Font Size" />
                     < Button text="Translate" />     
 
