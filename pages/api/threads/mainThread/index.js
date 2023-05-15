f3f6dde85b4ece7766a1f6aaf9179dc7e8cdd12c
Noel kim <wkim51@my.bcit.ca>
2023-05-15 12:41:12 -0700

    finished genre search funtion but tag function not tested yet

--- pages/api/threads/mainThread/index.js
+++ pages/api/threads/mainThread/index.js
@@ -7,13 +7,16 @@ import {
 } from "@/lib/prisma/mainThreads";
 
 
+
 const handler = async (req, res) => {
   if (req.method === "GET") {
     try {
       const searchParam = req.query.search;
-      
-      const getMainThread = searchParam
-        ? getSearchMainThreads(searchParam)
+      const genreParam = req.query.genre;
+      const tagParam = req.query.tag;
+
+      const getMainThread = searchParam || genreParam || tagParam
+        ? getSearchMainThreads(searchParam, genreParam, tagParam)
         : getRandomMainThreads();
 
       const { randomMainThreads, error } = await getMainThread;
