a8a1c56ff3db1d5c95de2d97bd2c028c1cacfda8
Juan Chung <juanchung7v@gmail.com>
2023-05-16 13:21:43 -0700

    Added profile components

--- app/(site)/newProfile/page.jsx
+++ app/(site)/newProfile/page.jsx
@@ -1,68 +1,147 @@
 import React from "react";
+import Image from 'next/image';
 
-// classNameName="block
-// border
-// border-gray-400
-// rounded-lg
-// hover:shadow-md
-// hover:border-opacity-0
-// transform
-// transition-all duration-200
-// text-white-700
-// text-sm
-// text-center
-// font-mono
-// py-1
-// px-3"
+// penName, emailAddress
+// like > title, genre, status
+
+useEffect(() => {
+  const userId = 
+}
 
 const page = () => {
   return (
     <div>
       <div className="p-10 mt-20">
-        <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="grid grid-cols-1">
 
           <div className="relative">
-
             <div className="w-40 h-40 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-20 flex items-center justify-center text-indigo-500">
-            </div>
+          </div>
 
-            <div className="grid grid-cols-3 text-center">
+          <div className="mt-28 text-center pb-12">
+            <h1 className="text-4xl font-mono text-gray-700">Pen Name</h1>
+            <p className="font-mono text-gray-600 mt-3">Email Address</p>
+          </div>
+
+          <div className="grid grid-cols-3 text-center">
                 <div>
                 <p className="font-bold text-gray-700 text-xl">22</p>
-                <p className="text-gray-400">Friends</p>
+                <p className="text-gray-400 font-mono">Owned</p>
                 </div>
                 <div>
                 <p className="font-bold text-gray-700 text-xl">10</p>
-                <p className="text-gray-400">Photos</p>
+                <p className="text-gray-400 font-mono">Likes</p>
                 </div>
                 <div>
                 <p className="font-bold text-gray-700 text-xl">89</p>
-                <p className="text-gray-400">Comments</p>
+                <p className="text-gray-400 font-mono">Collaborated</p>
                 </div>
-            </div>
-          
-
-            <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
-              <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                Connect
-              </button>
-              <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                Message
-              </button>
-            </div>
           </div>
 
-          <div className="mt-20 text-center border-b pb-12">
-            <h1 className="text-4xl font-medium text-gray-700">
-              Jessica Jones,{" "}
-              <span className="font-light text-gray-500">27</span>
-            </h1>
-            <p className="font-light text-gray-600 mt-3">Bucharest, Romania</p>
+          <div className="mt-10 pb-5">
+              <h1 className="text-xl font-mono text-gray-700">Owned Thread</h1>
+              <hr className="mt-1"></hr>
+          </div>
 
-            <p className="mt-8 text-gray-500">
-              Solution Manager - Creative Tim Officer
-            </p>
-            <p className="mt-2 text-gray-500">University of Computer Science</p>
+            <div className="flex overflow-x-scroll gap-2 w-full">
+
+              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+              </a>
+
+              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+              </a>
+
+              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+              </a>
+
+              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+              </a>
+
+              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+              </a>
+
+              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+              </a>
+
+          </div>
+
+          <div className="grid grid-cols-2 mt-10 pb-5">
+              <h1 className="text-xl font-mono text-gray-700">Likes Thread</h1>
+              {/* <button className="text-white py-1 px-2 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                Complete
+              </button> */}
+              <div class="inline-flex">
+              <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-mono py-1 px-4 rounded-l">
+                Complete
+              </button>
+              <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-mono py-1 px-4 rounded-r">
+                Incomplete
+              </button>
+          </div>
+              <hr className="mt-1"></hr>
+          </div>
+
+          <div className="flex overflow-x-scroll gap-2 w-full">
+
+            <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+            </a>
+
+            <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+            </a>
+
+            <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+            </a>
+
+            <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+            </a>
+
+            <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+            </a>
+
+            <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
+                <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
+                <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
+            </a>
+
+            </div>
+
+
+
+
           </div>
 
           <div className="mt-12 flex flex-col justify-center">
