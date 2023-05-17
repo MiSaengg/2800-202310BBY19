9db661f1ca7cfb6aa1e4d64ef8559993ee2ac648
Juan Chung <juanchung7v@gmail.com>
2023-05-17 00:31:19 -0700

    Fetched data from Mainthreads and added a complete/incomplete button

--- app/(site)/newProfile/page.jsx
+++ app/(site)/newProfile/page.jsx
@@ -13,10 +13,11 @@ export default function Profile() {
   const [email, setEmail] = useState("");
 
   const [likeThreads, setlikeThreads] = useState([]);
+  const [mainThreads, setMainThreads] = useState([]);
+  
   const [title, setTitle] = useState("");
   const [genre, setGenre] = useState([]);
   const [tag, setTag] = useState("");
-
   
 
   // const [like, setLike] = useState([]);
@@ -40,12 +41,26 @@ export default function Profile() {
         const value = Object.values(data);
         setlikeThreads(value);
       })
-  })
+  }, []);
 
   useEffect(() => {
     const userId = localStorage.getItem("userID")
     const endpoint = `/api/profile?userId=${userId}`;
-  })
+    console.log(endpoint);
+
+    fetch(endpoint, {
+      method: "GET"
+    })
+      .then((res) => res.json())
+      .then(({ mainThreads }) => {
+        console.log(mainThreads)
+        setTitle(mainThreads.title);
+        setGenre(mainThreads.genre);
+        setTag(mainThreads.tag);
+        setMainThreads(mainThreads);
+      })
+    }, []);
+
 
   return (
     <div>
@@ -76,49 +91,16 @@ export default function Profile() {
                 </div>
           </div>
 
+          <fieldset className="mt-10 grid grid-cols-2 justify-items-start">
+            <legend className="mb-2 font-lg font-bold">Thread status</legend>
+            <label for="complete" class="peer-checked/complete:text-sky-500">Complete</label>
+            <input id="complete" class="peer/complete" type="radio" name="status" checked />
+            <label for="incomplete" class="peer-checked/completed:text-sky-500">Incomplete</label>
+            <input id="incomplete" class="peer/incompleted" type="radio" name="status" />
+          </fieldset>
+
           <div className="mt-10 pb-5">
-              <h1 className="text-xl font-mono text-gray-700">Owned Thread</h1>
-              <hr className="mt-1"></hr>
-          </div>
-
-            <div className="flex overflow-x-scroll gap-2 w-full">
-
-              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
-              </a>
-
-              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
-              </a>
-
-              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
-              </a>
-
-              <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
-                  <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">This is Noel's love story</h5>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Sci-Fi</p>
-                  <p className="mb-1 text-xs font-mono text-gray-700">Complete</p>
-              </a>
-
-          </div>
-
-          <div className="grid grid-cols-2 mt-10 pb-5">
               <h1 className="text-xl font-mono text-gray-700">Likes Thread</h1>
-              <div class="inline-flex">
-              <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-mono py-1 px-4 rounded-l">
-                Complete
-              </button>
-              <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-mono py-1 px-4 rounded-r">
-                Incomplete
-              </button>
-          </div>
               <hr className="mt-1"></hr>
           </div>
 
@@ -134,18 +116,27 @@ export default function Profile() {
 
           </div>
 
-
-
-
+          <div className="mt-10 pb-5">
+              <h1 className="text-xl font-mono text-gray-700">Owned Thread</h1>
+              <hr className="mt-1"></hr>
           </div>
 
-          <div className="mt-12 flex flex-col justify-center">
-            <h1 className="text-gray-600 text-center lg:px-16">
-              Be yourself; everyone else is already taken.
-            </h1>
-            <button className="text-indigo-500 py-2 px-4 mt-4">
-              Show more
-            </button>
+            <div className="flex overflow-x-scroll gap-2 w-full">
+
+              {mainThreads.map((b, i) => (
+                <a href="#" className="flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100" key={i}>
+                    <h5 className="mb-1 text-xs font-bold font-mono tracking-tight text-gray-700">{b.title}</h5>
+                    <p className="mb-1 text-xs font-mono text-gray-700">{b.genre}</p>
+                    <p className="mb-1 text-xs font-mono text-gray-700">{b.tag}</p>
+                </a>
+              ))}
+
+            </div>
+
+
+
+
+
           </div>
         </div>
       </div>
