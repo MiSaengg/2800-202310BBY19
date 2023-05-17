31a196a7c15bb92e41aa0d1c7ac5f0eb261dd288
Juan Chung <juanchung7v@gmail.com>
2023-05-17 15:00:13 -0700

    Deleted temporary texts on the complete reading page

--- app/components/box/RReadTextBox.jsx
+++ app/components/box/RReadTextBox.jsx
@@ -30,22 +30,19 @@ const RReadTextBox = ({ body, image, phase }) => {
 
     <div class="ml-10 rounded-l-lg bg-white shadow-xl">
       <div class="flex flex-cols-2 border-b-2 leading-relaxed border-neutral-100 px-6 py-2">
-      <div>
-        <Image
-          src={image}
-          width={30}
-          height={30}
-          alt="ProfilePhoto"
-          className="object-cover rounded-lg mr-10"
-        />
-      </div>
-      Phase {phase}
+        <div>
+          <Image
+            src={image}
+            width={30}
+            height={30}
+            alt="ProfilePhoto"
+            className="object-cover rounded-lg mr-10"
+          />
+        </div>
+        Phase {phase}
       </div>
       <div class="p-6">
-        <p class="mb-4 text-base text-neutral-600">
-          {/* {body} */}
-          Lorem ipsum dolor sit amet. Et veritatis tempore non corrupti maxime qui molestias saepe ea illo magni eum quidem necessitatibus a molestiae dolor aut deserunt quia. Est quia libero sed maiores sapiente ut itaque eligendi. Quo quia laborum est vitae quia qui optio suscipit est error mollitia ut tenetur nostrum non voluptatem unde?
-        </p>
+        <p class="mb-4 text-base text-neutral-600">{body}</p>
       </div>
     </div>
   );
