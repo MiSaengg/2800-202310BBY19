49fcf026037de9930c356401e18232360f70a3a9
Jasper Oh <ohyj0906@gmail.com>
2023-05-17 21:29:01 -0700

    Spike handling 1

--- app/(site)/profile/layout.jsx
+++ app/(site)/profile/layout.jsx
@@ -1,18 +1,4 @@
-import Users from "./Users"
-import { getUsers } from "@/lib/prisma/users"
-
-
-export const revalidate = 0
-
-async function getData(){
-  const {users} = await getUsers()
-  if(!users){
-    throw new Error("Failed to fetch data")
-  }
-}
-
-const UsersLayout = async ({ children }) => {
-  const users = await getData()
+const UsersLayout = ({ children }) => {  
 
   return (
     <div>{children}</div>
