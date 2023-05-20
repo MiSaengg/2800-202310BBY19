70014f825f5b3adce2d0b7be0b7a07046b3c6351
David Cho <gcho13@my.bcit.ca>
2023-05-19 22:50:22 -0700

    penName

--- app/newUser/page.jsx
+++ app/newUser/page.jsx
@@ -46,13 +46,13 @@ export default function AddPenName() {
   return (
     <main className='flex min-h-full overflow-hidden pt-16 sm:py-28'>
       <div className='mx-auto flex w-full max-w-2xl flex-col px-4 sm:px-6'>
-        <Link
+        {/* <Link
           href='/'
           className='flex items-center gap-2 text-stone-500 hover:text-stone-800 transition-colors'
         >
         <ChevronLeftIcon className='w-5' />
           <span className='uppercase font-medium'>Home</span>
-        </Link>
+        </Link> */}
       <div className='relative mt-12 sm:mt-16'>
         <h1 className='text-center text-2xl font-medium tracking-tight text-gray-900'>
           Enter Your Pen Name !          
