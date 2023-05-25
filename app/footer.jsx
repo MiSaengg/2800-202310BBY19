da8bb563152cf70216ba194c829067f5375c95bf
David Cho <gcho13@my.bcit.ca>
2023-05-25 12:57:41 -0700

    Clicking home button resets genre

--- app/footer.jsx
+++ app/footer.jsx
@@ -1,50 +1,53 @@
 // Because we're using react thing in line 4.
 "use client";
 import { useSession } from "next-auth/react";
-import Link from 'next/link';
-import { HomeIcon, PlusCircleIcon, UserIcon } from '@heroicons/react/24/outline'
+import Link from "next/link";
+import {
+  HomeIcon,
+  PlusCircleIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 
-
-export default function Footer(){
+export default function Footer() {
   const { data: session } = useSession();
 
-  const [userId, setUserId] = useState(null)
+  const [userId, setUserId] = useState(null);
 
-  useEffect(()=> {
-    const userID = localStorage.getItem("userID") 
-    setUserId(userID)
-  }, [userId])
+  useEffect(() => {
+    const userID = localStorage.getItem("userID");
+    setUserId(userID);
+  }, [userId]);
 
   if (session) {
     return (
       <footer className="z-10 bg-stone-100 py-4 text-center">
         <div className="flex items-center justify-between mx-10">
-          <Link
+          <a
             href="/threads"
             className="text-stone-500 hover:text-stone-800 transition-colors"
           >
             <HomeIcon className="w-9" />
             <span className="uppercase font-mono text-xs">Home</span>
-          </Link>
-          <Link
+          </a>
+          <a
             href="/createMainThread"
             className="text-stone-500 hover:text-stone-800 transition-colors"
           >
             <PlusCircleIcon className="w-9" />
-            <span className="uppercase font-mono text-xs">Start</span>
-          </Link>
-          <Link
+            <span className="uppercase font-mono text-xs">Create</span>
+          </a>
+          <a
             href={`/profile`}
             className="text-stone-500 hover:text-stone-800 transition-colors"
           >
             <UserIcon className="w-9" />
             <span className="uppercase font-mono text-xs">Profile</span>
-          </Link>
+          </a>
         </div>
       </footer>
     );
   }
 
   return;
-};
+}
