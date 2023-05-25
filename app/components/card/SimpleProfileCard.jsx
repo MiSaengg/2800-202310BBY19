d6b8a24c696c0ff18838d9cfe46af966fa98e02f
David Cho <gcho13@my.bcit.ca>
2023-05-24 19:28:05 -0700

    Removed dark mode

--- app/components/card/SimpleProfileCard.jsx
+++ app/components/card/SimpleProfileCard.jsx
@@ -181,9 +181,9 @@ const SimpleProfileCardInfo = ({
           className="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto max-h-full"
         >
           <div className="relative max-w-full max-h-full">
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              <div className="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-xl font-mono text-gray-900 dark:text-white">
+            <div className="relative bg-white rounded-lg shadow">
+              <div className="flex items-start justify-between p-5 border-b rounded-t">
+                <h3 className="text-xl font-mono text-gray-900">
                   Title
                 </h3>
                 <VotesCompleteButton
@@ -196,7 +196,7 @@ const SimpleProfileCardInfo = ({
                 <button
                   onClick={closeModalEvent}
                   type="button"
-                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
                   data-modal-hide="defaultModal"
                 >
                   <svg
@@ -217,25 +217,25 @@ const SimpleProfileCardInfo = ({
               </div>
 
               <div className="p-6 space-y-6">
-                <p className="text-base font-mono text-gray-700 dark:text-gray-400">
+                <p className="text-base font-mono text-gray-700">
                   Branch Story
                 </p>
               </div>
               <form onSubmit={mergeBranchThreadToMainThread}>
                 <label
                   htmlFor="text-input"
-                  className="block mb-3 text-md font-mono text-gray-900 dark:text-white"
+                  className="block mb-3 text-md font-mono text-gray-900"
                 >
                   <textarea
                     name="branchContext"
                     id="text-input"
                     rows="15"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     readOnly
                     value={branchText}
                   ></textarea>
                 </label>
-                <div className="flex items-stretch justify-between px-5 pt-1 pb-3 border-gray-200 rounded-b dark:border-gray-600">
+                <div className="flex items-stretch justify-between px-5 pt-1 pb-3 border-gray-200 rounded-b">
                   {currentUserId === userId || currentUserId === ownerUserId ? (
                     <Button
                       type="button"
